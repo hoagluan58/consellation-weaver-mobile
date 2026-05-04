aa0df34ba5722f933bdf5443e45d43bb160beb81
Po <>
2026-05-05 00:01:08 +0700

    feat: remove unity project

--- Assets/Nframework/Assets/Scripts/Helper/EditorHelper.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace NFramework
-{
-    public static class EditorHelper
-    {
-        public static string GetUniqueProjectPrefsKey(string key) => $"{Application.dataPath}_{key}";
-    }
-}
--- Assets/Nframework/Assets/Scripts/Singleton/Singleton.cs
+++
@@ -1,7 +0,0 @@
-namespace NFramework
-{
-    public class Singleton<T> where T : class, new()
-    {
-        public static T I { get; } = new T();
-    }
-}
